ca6b06154ea48e09cd1c3e2b3beddf38db56165d
Anvesha Srivastava <207409304+Anvesha-Sri@users.noreply.github.com>
2026-05-21 00:37:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,14 +75,14 @@ searchBtn.onclick = function() {
                 lon: data.coord.lon
             };
             let weatherImages ={
-                cloud: ".github/cloud.png",
-                haze: ".github/haze.png",
-                rain: ".github/rain.png",
-                snow: ".github/snow.png",
-                sun: ".github/sun.png",
-                thunderstorm: ".github/rain.png",
-                mist: ".github/haze.png",
-                fog: ".github/haze.png",
+                cloud: "assets/cloud.png", 
+                haze: "assets/haze.png",
+                rain: "assets/rain.png",
+                snow: "assets/snow.png",
+                sun: "assets/sun.png",
+                thunderstorm: "assets/rain.png",
+                mist: "assets/haze.png",
+                fog: "assets/haze.png",
             };
             let weatherImageSrc = weatherImages[weatherCondition] || "./assets/intro.png";
             weather.innerHTML = `
